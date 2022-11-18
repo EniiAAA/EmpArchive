394e394322a9839b1927688264b8f7a114eb98f0
Elvio Júnior <elvio.vieira95@gmail.com>
2022-11-18 08:24:15 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
 			
 			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.setTitle("EmpArchive");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
